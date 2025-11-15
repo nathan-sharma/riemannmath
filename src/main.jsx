71fad89e7d35a7ec33dbaeaf0aa09a7378b04b4e
Nathan Sharma <nathansharma007@gmail.com>
2025-11-15 12:49:23 -0600

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,20 @@
-if (sessionStorage.redirected) {
-  sessionStorage.removeItem('redirected');
-  console.log("Redirected from 404, now handling route.");
-}
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { HashRouter, Routes, Route } from 'react-router-dom'; 
 import './index.css';
 import App from './App';
-import Register from './Register.jsx'
-import Info from './Info.jsx'
+import Register from './Register.jsx';
+import Info from './Info.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> 
-      <Routes> 
+    <HashRouter>
+      <Routes>
         <Route path="/" element={<App />} /> 
-        <Route path="/registration" element={<Register/>} />
-        <Route path = "/info-and-about" element ={<Info/>}  />
+        <Route path="/registration" element={<Register />} />
+        <Route path="/info-and-about" element={<Info />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
