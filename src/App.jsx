289406f5809a14ba7d1aa0fafd743b895380c882
Nathan Sharma <nathansharma007@gmail.com>
2025-11-26 10:39:51 -0600

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
             {/* Left text */}
             <div className="flex-1">
               <p className="text-left text-gray-900 text-xl md:text-3xl leading-snug md:leading-relaxed font-sans">
-                Riemann Math is a <span className = "font-bold">completely free</span> online math tutoring service based in Katy, Texas, made for young math students. Our curriculum encompasses a broad range of advanced concepts, many of which are not thoroughly covered in the standard high school curriculum.
+                Riemann Math is a <span className = "font-bold">completely free</span> online competitive math tutoring service based in Katy, Texas, made for young math students. Our curriculum encompasses a broad range of advanced concepts, helping our students prepare for competitions such as the AMC 8, 10, 12, AIME, MATHCOUNTS, and more!
               </p>
             </div>
 
